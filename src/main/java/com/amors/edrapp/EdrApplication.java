5eb23827a8a39474dac02f021d222ca4271e82cc
theatramors <themaliciousman@gmail.com>
2019-06-29 04:12:24 +0300

    updated profiles

--- src/main/java/com/amors/edrapp/EdrApplication.java
+++ src/main/java/com/amors/edrapp/EdrApplication.java
@@ -2,9 +2,7 @@ package com.amors.edrapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
-@Profile(value = "develop")
 @SpringBootApplication
 public class EdrApplication {
     public static void main(String[] args) {
