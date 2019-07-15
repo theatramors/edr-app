6f84e148eeb9f748d5c4293d0564eb87217ca76b
theatramors <themaliciousman@gmail.com>
2019-07-15 10:50:40 +0300

    added GlobalExceptionHandler

---
+++ src/main/java/com/amors/edrapp/aop/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.amors.edrapp.aop;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+}
