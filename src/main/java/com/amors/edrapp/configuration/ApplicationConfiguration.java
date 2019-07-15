9b69f91d3f2846b45dc0193c452ebd23c810991c
theatramors <themaliciousman@gmail.com>
2019-07-15 16:03:16 +0300

    added conditional datasource configuration

--- src/main/java/com/amors/edrapp/configuration/ApplicationConfiguration.java
+++ src/main/java/com/amors/edrapp/configuration/ApplicationConfiguration.java
@@ -1,8 +1,14 @@
 package com.amors.edrapp.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+import static org.springframework.boot.jdbc.DataSourceBuilder.create;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -12,5 +18,16 @@ public class ApplicationConfiguration {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.findAndRegisterModules();
         return objectMapper;
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "spring.datasource", name = "enabled", havingValue = "true")
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/edr-develop");
+        dataSourceBuilder.username("edr");
+        dataSourceBuilder.password("edr");
+        return dataSourceBuilder.build();
     }
 }
