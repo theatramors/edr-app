7999a6a0761c18c88ca4cc360f51d4ebcbfdacd5
theatramors <themaliciousman@gmail.com>
2019-07-15 16:00:50 +0300

    updated WebSecurityConfiguration

--- src/main/java/com/amors/edrapp/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/amors/edrapp/configuration/WebSecurityConfiguration.java
@@ -9,7 +9,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
         http.authorizeRequests().antMatchers("/**").permitAll();
     }
 }
