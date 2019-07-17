6217ba52507edd39fc5587f3d59bc65f5520d012
theatramors <themaliciousman@gmail.com>
2019-07-17 11:17:44 +0300

    added MultipartProperties bean

--- src/main/java/com/amors/edrapp/configuration/ApplicationConfiguration.java
+++ src/main/java/com/amors/edrapp/configuration/ApplicationConfiguration.java
@@ -1,8 +1,11 @@
 package com.amors.edrapp.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.springframework.util.unit.DataSize.parse;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -12,5 +15,13 @@ public class ApplicationConfiguration {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.findAndRegisterModules();
         return objectMapper;
+    }
+
+    @Bean
+    public MultipartProperties multipartProperties() {
+        MultipartProperties properties = new MultipartProperties();
+        properties.setMaxFileSize(parse("25MB"));
+        properties.setMaxRequestSize(parse("25MB"));
+        return properties;
     }
 }
