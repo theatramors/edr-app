4628fa8c1d24fa8563efc0db82de5cb1053f6e77
theatramors <themaliciousman@gmail.com>
2019-07-20 12:56:13 +0300

    added postConstruct() to UserService

--- src/main/java/com/amors/edrapp/services/UserService.java
+++ src/main/java/com/amors/edrapp/services/UserService.java
@@ -3,6 +3,7 @@ package com.amors.edrapp.services;
 import com.amors.edrapp.dto.UserDto;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,10 +14,11 @@ public class UserService {
 
     private Map<String, UserDto> userDtoMap;
 
-    public UserService() {
+    @PostConstruct
+    public void postConstruct() {
         userDtoMap = new HashMap<String, UserDto>() {{
-           put("John", UserDto.builder().username("John").email("john@mail.com").build());
-           put("Mike", UserDto.builder().username("Mike").email("mike@mail.com").build());
+            put("John", UserDto.builder().username("John").email("john@mail.com").build());
+            put("Mike", UserDto.builder().username("Mike").email("mike@mail.com").build());
         }};
     }
 
