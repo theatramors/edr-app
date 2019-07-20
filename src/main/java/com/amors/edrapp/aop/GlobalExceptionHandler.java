4b6f2ba7257497e35f77fc365f2d6d659d37415a
theatramors <themaliciousman@gmail.com>
2019-07-20 13:04:56 +0300

    added handlers to GlobalExceptionHandler

--- src/main/java/com/amors/edrapp/aop/GlobalExceptionHandler.java
+++ src/main/java/com/amors/edrapp/aop/GlobalExceptionHandler.java
@@ -2,10 +2,14 @@ package com.amors.edrapp.aop;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
@@ -14,6 +18,16 @@ import javax.persistence.EntityNotFoundException;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleHttpRequestMethodNotSupported(ex, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleHttpMediaTypeNotSupported(ex, headers, status, request);
+    }
 
     @ExceptionHandler(EntityNotFoundException.class)
     protected ResponseEntity handleEntityNotFoundException(EntityNotFoundException ex) {
