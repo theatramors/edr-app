87bdccb336eefe6858ba43bc908a30cf1137ab30
theatramors <themaliciousman@gmail.com>
2019-07-24 13:42:18 +0300

    added kafka configuration template

---
+++ src/main/java/com/amors/edrapp/configuration/KafkaConfiguration.java
@@ -0,0 +1,9 @@
+package com.amors.edrapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConfiguration {
+}
